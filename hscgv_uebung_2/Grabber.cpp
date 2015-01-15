a66d61eec1d1c4e30d0a956cb2856d2e3b6f2da7
Jeronim Morina <plattenschieber@gmail.com>
2015-01-15 16:53:35 +0100

    update face indices

--- hscgv_uebung_2/Grabber.cpp
+++ hscgv_uebung_2/Grabber.cpp
@@ -139,21 +139,14 @@ static float shoulderVertices[12][3] =
 
 static int shoulderIndices[44] =
 {
-   1,  2,  3,  4, 5, SO_END_FACE_INDEX, // top face
-
-   0,  1,  8,  7, 3, SO_END_FACE_INDEX, // 5 faces about top
-   0,  2,  7,  6, 4, SO_END_FACE_INDEX,
-   0,  3,  6, 10, 5, SO_END_FACE_INDEX,
-   0,  4, 10,  9, 1, SO_END_FACE_INDEX,
-   0,  5,  9,  8, 2, SO_END_FACE_INDEX,
-
-    9,  5, 4, 6, 11, SO_END_FACE_INDEX, // 5 faces about bottom
-   10,  4, 3, 7, 11, SO_END_FACE_INDEX,
-    6,  3, 2, 8, 11, SO_END_FACE_INDEX,
-    7,  2, 1, 9, 11, SO_END_FACE_INDEX,
-    8,  1, 5,10, 11, SO_END_FACE_INDEX,
-
-    6,  7, 8, 9, 10, SO_END_FACE_INDEX, // bottom face
+    1, 2, 3,  4,  5, SO_END_FACE_INDEX,    // bottom face
+    6, 7, 8, 9, 10, 11, SO_END_FACE_INDEX, // top face
+    0, 1,  7,  6, SO_END_FACE_INDEX,       // side faces
+    1, 2,  8,  7, SO_END_FACE_INDEX,       // |
+    2, 3,  9,  8, SO_END_FACE_INDEX,       // |
+    3, 4, 10,  9, SO_END_FACE_INDEX,       // |
+    4, 5, 11, 10, SO_END_FACE_INDEX,       // |
+    5, 0,  6, 11, SO_END_FACE_INDEX,       // |
 };
 
 // Colors for the 12 faces
