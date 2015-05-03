c9c4f513edd691fe500dfc715085c343de8ff33d
Jeronim Morina <plattenschieber@gmail.com>
2015-05-03 13:58:29 +0200

    insert window into main application

--- hscgv_uebung_5/main.cpp
+++ hscgv_uebung_5/main.cpp
@@ -19,6 +19,10 @@
 #include "param.h"
 #include "types.h"
 
+// get definition of our application window class called ApplicationWindow
+#include "ApplicationWindow.h"
+#include <QApplication>
+
 #ifndef M_PI
 #define M_PI 3.1415927
 #endif
@@ -97,6 +101,42 @@ extern std::vector<LightObject*>         g_lightList;
 //! the list of surface properties of the objects
 extern std::vector<GeoObjectProperties*> g_propList;
 
+int startWindow(int argc, char *argv[])
+{
+    // use true color visual on SGIs
+    QApplication::setColorSpec(QApplication::ManyColor);
+
+    // create application object and pass all the arguments.
+    // this will filter out all Qt-relevant commands and returns
+    // a modified command line (containing your options).
+    QApplication app(argc,argv);
+
+    // create main widget of our application.
+    // you have to pass the application object.
+    ApplicationWindow *win = new ApplicationWindow();
+
+    // set an initial size of the main window (not too small)
+    win->resize(500,300);
+
+    // show the window
+    win->show();
+
+    // if the last window (here the app window) gets closed by the
+    // user (i.e. by clicking on the close button of the window frame) then
+    // the window will emit a "lastWindowClosed" signal. We connect this
+    // signal with the "quit" slot of the application object. On quit the
+    // application will leave the event loop started below.
+    app.connect( &app, SIGNAL(lastWindowClosed()), &app, SLOT(quit()) );
+
+    // enter main event loop of the Qt application object.
+    // this object will reveive all mesages from the GUI and pass them
+    // along to our widgets (e.g. the main application window). These
+    // events are called signals and they trigger the slot-methods they
+    // are connected to.
+    // if you trigger the "quit" slot of the application object then the
+    // method call will return.
+    return app.exec();
+}
 
 // Description:
 /*!
@@ -109,6 +149,7 @@ extern std::vector<GeoObjectProperties*> g_propList;
 int
 main (int argc, char *argv[])
 {
+   startWindow(argc, argv);
    int c;
    bool haveFilename = false, antialiasing = false;
    char filename[MAXNAMLEN - 5] = "";
