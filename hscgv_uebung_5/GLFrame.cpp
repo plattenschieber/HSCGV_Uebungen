bc79de69f6bc63d18fc80a62f1f2f50bdb8c7f90
Jeronim Morina <plattenschieber@gmail.com>
2015-05-05 17:56:39 +0200

    generate an example checkerboard texture

--- hscgv_uebung_5/GLFrame.cpp
+++ hscgv_uebung_5/GLFrame.cpp
@@ -87,6 +87,15 @@ void GLFrame::loadTexture()
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_BORDER);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_BORDER);
 
+    // generate the texture
+    unsigned char num[4*3] = { 0x0, 0x0, 0x0,
+                      0xff, 0xff, 0xff,
+                      0xff, 0xff, 0xff,
+                      0x0, 0x0, 0x0 };
+
+    unsigned char num2[2*2] = { 0xff, 56, 128, 0x0};
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid*)m_userData);CHECKGL;
+
 }
 
 
